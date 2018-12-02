6b008162f77ca64c910ea493060ea0e6c179b2b8
dornby <12762571+dornby@users.noreply.github.com>
2018-12-02 23:16:49 +0100

    city randomization and API on weather v1

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,13 @@
 class SearchController < ApplicationController
 
-  def index
-  end
-
   def create
     @search = Search.new
     @search.save
+  end
+
+  def show
+    create
+    p @search.pick_random_destination
   end
 
 end
--- app/models/search.rb
+++ app/models/search.rb
@@ -1,2 +1,8 @@
 class Search < ApplicationRecord
+
+  def pick_random_destination
+    city = Cities.new.pick_random_city
+    p city["name"]
+  end
+
 end
